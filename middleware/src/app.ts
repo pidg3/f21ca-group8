d511338a27912b1fa48c75f158c5514bdf710485
Michael Pidgeon <michael.r.pidgeon@gmail.com>
2021-02-09 11:12:17 +0000

    Middleware pipeline and simple fastify server

--- middleware/src/app.ts
+++ middleware/src/app.ts
@@ -1,5 +1,40 @@
-function foo(): string {
-  return 'Bar';
-}
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
 
-export { foo };
+const server: FastifyInstance = Fastify({
+  logger: true
+});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          resp: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+};
+
+server.get('/ping', opts, async (request, reply) => {
+  return { resp: 'it worked!' };
+});
+
+server.get('/', opts, async (request, reply) => {
+  return { resp: 'root!' };
+});
+
+
+const start = async () => {
+  try {
+    await server.listen(process.env.PORT || 3000, '127.0.0.1');
+
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
