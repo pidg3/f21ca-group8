54e391ed360159e8837c2dd5543fcad25a72b34b
km184 <55809503+km184@users.noreply.github.com>
2021-02-16 02:43:23 +0200

    Add files via upload
    Simple code latest

--- actions.py
+++ actions.py
@@ -15,9 +15,53 @@ from rasa_sdk.executor import CollectingDispatcher
 class actionListen(Action):
 
     def name(self) -> Text:
-        return "Kuda_custom"
+        return "CustomAction"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Hello World! kuda in the building")
+        entities = tracker.latest_message['entities']
+        print('latest action:', tracker.latest_action_name)
+        print("----------------------------------------------------")
+        print(tracker.latest_message)
+        print(entities)
+        for e in entities:
+            if e['entity'] == 'key':
+                name = e['value']
+                print(name)
+
+        dispatcher.utter_message(
+            text="Wow I would love to visit some day. {0} is one of the amazing places. How is {0}?.".format(name))
+        return []
+
+
+class actionListen(Action):
+
+    def name(self) -> Text:
+        return "custom_listen"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message['entities']
+        print(tracker.latest_message)
+        name = 'Speak'
+        for e in entities:
+            if e['entity'] == 'key':
+                name = e['value']
+                print('-------------------------------------------------------------')
+                print(name)
+        if name != 'Speak':
+            dispatcher.utter_message(text="I need to shut up")
+        else:
+            dispatcher.utter_message(text='I may speak')
+        return []
+
+
+class actionNoResponse(Action):
+
+    def name(self) -> Text:
+        return "no_response"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        pass
         return []
