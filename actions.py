46355c53e7dcba89be4cab6a40d00030cad58598
km184 <55809503+km184@users.noreply.github.com>
2021-02-27 16:02:02 +0200

    Add files via upload

--- actions.py
+++ actions.py
@@ -15,22 +15,37 @@ from rasa_sdk.executor import CollectingDispatcher
 class actionListen(Action):
 
     def name(self) -> Text:
-        return "CustomAction"
+        return "action_CustomAction"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         entities = tracker.latest_message['entities']
         print('latest action:', tracker.latest_action_name)
-        print("----------------------------------------------------")
         print(tracker.latest_message)
-        print(entities)
-        for e in entities:
-            if e['entity'] == 'key':
-                name = e['value']
-                print(name)
 
-        dispatcher.utter_message(
-            text="Wow I would love to visit some day. {0} is one of the amazing places. How is {0}?.".format(name))
+        print(entities)
+        countries_cities = {
+            "Botswana": "Gaborone",
+            "India": "Mumbai",
+
+        }
+        print('-----------------------------')
+        print(tracker.events[-3].get('text'), "Kuda attempting something")
+        print('-----------------------------')
+        cs = tracker.get_slot('location')
+        if cs == 'mumbai':
+            dispatcher.utter_message(text="correct")
+        else:
+            dispatcher.utter_message(text='NOPE!')
+            rasa_sdk.events.SlotSet(key="location", value="BOTLOC")
+        print(cs, 'hereeeeeeeeeeeeeeeee')
+
+        # for e in entities:
+        #  if e['entity'] == 'key':
+        #   name = e['value']
+        #   print(name)
+
+
         return []
 
 
@@ -64,4 +79,27 @@ class actionNoResponse(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         pass
+        return []
+
+
+class QuizTime(Action):
+    """Pay credit card."""
+
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+        return "action_Qiuz"
+
+    async def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Executes the action"""
+
+        city_answer = tracker.get_slot("city")
+        if city_answer == "Gaborone":
+            dispatcher.utter_message(text="Good!")  # template defined in the domain file
+        else:
+            dispatcher.utter_message(text="Oh, No!")  # template defined in the domain file
         return []
